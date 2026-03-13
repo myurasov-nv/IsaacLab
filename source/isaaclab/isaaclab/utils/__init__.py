a9a43e8a4f8dbda49f498d0b64e3bd4bbe82fc27
Mikhail Yurasov <myurasov@nvidia.com>
2026-03-13 01:55:31 -0700

    Make lazy_loader import optional to avoid ModuleNotFoundError

--- source/isaaclab/isaaclab/utils/__init__.py
+++ source/isaaclab/isaaclab/utils/__init__.py
@@ -7,6 +7,9 @@
 
 from .configclass import configclass
 
-import lazy_loader as lazy
+try:
+    import lazy_loader as lazy
 
-__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
+    __getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
+except ImportError:
+    pass
